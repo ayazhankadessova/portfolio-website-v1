df0febe2fa42e34dca8ea1bd02055c1d0e265424
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-07-14 14:39:53 +0800

    add: init Contact section

--- portfolio-website/src/components/contact/Contact.js
+++ portfolio-website/src/components/contact/Contact.js
@@ -1,8 +1,43 @@
 import React from 'react'
 import './contact.css'
+import contactData from './contact_data'
 
 const Contact = () => {
-  return <section id='contact'>Contact</section>
+  return (
+    <section id='contact'>
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          {contactData.map((props) => {
+            return (
+              <article key={props.id} className='contact__item'>
+                {/* <div className='contact__item-image'>
+                  <img src={props.img} alt={props.title} />
+                </div> */}
+                <h3>{props.title}</h3>
+                <h4>{props.text}</h4>
+                <div className='contact__item-links'>
+                  <a
+                    href={props.link}
+                    className='btn btn-primary'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    Send a message
+                  </a>
+                </div>
+              </article>
+            )
+          })}
+        </div>
+
+        {/* {END OF CONTACT OPTIONS} */}
+        <form action=''>HEY</form>
+      </div>
+    </section>
+  )
 }
 
 export default Contact
---
+++ portfolio-website/src/components/contact/contact_data.js
@@ -0,0 +1,22 @@
+const contactData = [
+  {
+    id: 1,
+    title: 'Email',
+    text: 'kadessovaayazhan@gmail.com',
+    link: 'https://github.com/',
+  },
+  {
+    id: 2,
+    title: 'Messenger',
+    text: '@ayazhankad07',
+    link: 'https://github.com/',
+  },
+  {
+    id: 3,
+    title: 'WhatsApp',
+    text: '9288-8547',
+    link: 'https://github.com/',
+  },
+]
+
+export default contactData
