10c433191ea1592b8b9c0cc7b563844e8be5bc85
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-07-14 23:20:08 +0800

    add: contact

--- portfolio-website/src/components/contact/Contact.js
+++ portfolio-website/src/components/contact/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './contact.css'
 import contactData from './contact_data'
+import { MdEmail } from 'react-icons/md'
 
 const Contact = () => {
   return (
@@ -13,18 +14,11 @@ const Contact = () => {
           {contactData.map((props) => {
             return (
               <article key={props.id} className='contact__item'>
-                {/* <div className='contact__item-image'>
-                  <img src={props.img} alt={props.title} />
-                </div> */}
-                <h3>{props.title}</h3>
-                <h4>{props.text}</h4>
+                <div className='contact__icon'>{props.icon}</div>
+                <h4>{props.title}</h4>
+                <h5>{props.text}</h5>
                 <div className='contact__item-links'>
-                  <a
-                    href={props.link}
-                    className='btn btn-primary'
-                    target='_blank'
-                    rel='noreferrer'
-                  >
+                  <a href={props.link} target='_blank' rel='noreferrer'>
                     Send a message
                   </a>
                 </div>
@@ -34,7 +28,24 @@ const Contact = () => {
         </div>
 
         {/* {END OF CONTACT OPTIONS} */}
-        <form action=''>HEY</form>
+        <form action='' className='contact__form'>
+          <input
+            type='text'
+            name='Name'
+            placeholder='Your Full Name'
+            required
+          />
+          <input type='email' name='Email' placeholder='Your Email' required />
+          <textarea
+            name='message'
+            rows='7'
+            placeholder='Your Message'
+            required
+          ></textarea>
+          <button type='submit' className='btn btn-'>
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
   )
--- portfolio-website/src/components/contact/contact_data.js
+++ portfolio-website/src/components/contact/contact_data.js
@@ -1,21 +1,28 @@
+import { MdEmail } from 'react-icons/md'
+import { IoLogoLinkedin, IoLogoWhatsapp } from 'react-icons/io'
+import { AiOutlineLinkedin } from 'react-icons/ai'
+
 const contactData = [
+  {
+    id: 2,
+    title: 'Linkedin',
+    icon: <AiOutlineLinkedin />,
+    text: 'Ayazhan Kadessova',
+    link: 'https://www.linkedin.com/in/ayazhankad/',
+  },
   {
     id: 1,
     title: 'Email',
+    icon: <MdEmail />,
     text: 'kadessovaayazhan@gmail.com',
-    link: 'https://github.com/',
-  },
-  {
-    id: 2,
-    title: 'Messenger',
-    text: '@ayazhankad07',
-    link: 'https://github.com/',
+    link: 'mailto:kadessovaayazhan@gmail.com',
   },
   {
     id: 3,
     title: 'WhatsApp',
+    icon: <IoLogoWhatsapp />,
     text: '9288-8547',
-    link: 'https://github.com/',
+    link: 'https://api.whatsapp.com/send?phone=87778502000',
   },
 ]
 
